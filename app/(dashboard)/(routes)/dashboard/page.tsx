6fd49d64c076c601f488d5e3150614923e9bdecc
unknown <shivamnahar09@gmail.com>
2024-07-10 17:56:11 -0400

    addded the landing page structure

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+import {Button} from '@/components/ui/button'
+export default function DashboardPage() {
+  return (
+   <p className="text-6xl text-green-500">Hello AI SaaS
+   <Button variant={"destructive"} size={"lg"}>Click Me</Button>
+   </p>
+  );
+}
+
