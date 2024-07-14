def6c10350463fa9c0cf48d047e988d4e529b5b6
unknown <shivamnahar09@gmail.com>
2024-07-14 04:35:42 -0400

    Fixed bugs

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,24 +1,37 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import React, { useEffect, useState } from "react";
 import { Menu } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import SideBar from "@/components/sidebar";
+import Sidebar from "./sidebar";
 
-const MobileSidebar=()=>{
-    return(
-        <Sheet>
-            <SheetTrigger>
-                <Button variant={"ghost"} size={"icon"} className="md:hidden">
-                    <Menu/>
-                </Button>
-            </SheetTrigger>
-            <SheetContent side="left" className="p-0">
-                <SideBar/>
-            </SheetContent>
-        </Sheet>
-        
-    )
+export default function MobileSidebar({
+  apiLimitCount = 0,
+  isPro = false
+}: {
+  apiLimitCount: number;
+  isPro: boolean;
+}) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar/>
+      </SheetContent>
+    </Sheet>
+  );
 }
-
-export default MobileSidebar;
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -52,15 +52,15 @@ const routes=[
     },
 ]
 
-const SideBar=()=>{
+const Sidebar=()=>{
     return(
-        <div className="space-y-4 flex flex-col h-full bg-navy-700 text-white">
+        <div className="space-y-4 flex flex-col h-full bg-gray-800 text-white">
             <div className="px-3 py-2 flex-1">
                 <Link href={"/dashboard"} className="flex items-center pl-3 mb-14">
-                <div className="relative 2-8 h-8 mr-4">
+                <div className="relative w-8 h-8 mr-4">
                 <Image fill alt="logo" src="/logo.png" />
                 </div>
-                <h1 className={cn("text-2xl", monserrat.className)}><b>Genius</b></h1>
+                <h1 className={cn("text-2xl font-bold", monserrat.className)}>Genius</h1>
                 
                 </Link>
                 <div className="space-y-1">
@@ -81,4 +81,4 @@ const SideBar=()=>{
     )
 }
 
-export default SideBar;
+export default Sidebar;
